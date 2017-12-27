7f4ec9d546ad1c760478358235055a01d7aaa6dc
崇志 <chongzhi.ldq@taobao.com>
2017-12-27 13:55:48 +0800

    fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const execSync = require('child_process').execSync
 const path = require('path')
 
 module.exports = function (openUrl) {
-    let _path = path.resolve(process.cwd(), './node_modules/open-chrome/openChrome.applescript')
+    let _path = path.resolve(process.cwd(), './node_modules/auto-open-chrome/openChrome.applescript')
 
     //自动打开浏览器访问
     //会复用已经打开的标签页，不会一味的新窗口
