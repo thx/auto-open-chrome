7517386816d5a5d9699866cf5d8f6fa830820c2c
崇志 <chongzhi.ldq@taobao.com>
2017-12-27 13:32:53 +0800

    dev

---
+++ index.js
@@ -0,0 +1,14 @@
+const execSync = require('child_process').execSync
+const path = require('path')
+
+module.exports = function (openUrl) {
+    let _path = path.resolve(process.cwd(), './node_modules/open-chrome/openChrome.applescript')
+
+    //自动打开浏览器访问
+    //会复用已经打开的标签页，不会一味的新窗口
+    try {
+        execSync(`osascript ${_path} ${openUrl}`)
+    } catch (e) {
+        execSync(`open ${openUrl}`)
+    }
+}
