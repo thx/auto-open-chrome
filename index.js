9456d5c2168aeeb8a9ef397c22613793a410a0bc
崇志 <chongzhi.ldq@taobao.com>
2018-01-25 16:19:36 +0800

    Refactor: 优化cwd执行环境，__dirname

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const execSync = require('child_process').execSync
 const path = require('path')
 
 module.exports = function (openUrl) {
-    let _path = path.resolve(process.cwd(), './node_modules/auto-open-chrome/openChrome.applescript')
-
     //自动打开浏览器访问
     //会复用已经打开的标签页，不会一味的新窗口
     try {
-        execSync(`osascript ${_path} ${openUrl}`)
+        execSync(`osascript openChrome.applescript ${openUrl}`, {
+            cwd: __dirname
+        })
     } catch (e) {
         execSync(`open ${openUrl}`)
     }
